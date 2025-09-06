26e1b871a0910d5a1fabd6154fab8c100c6cf3d1
hafrey <56250572+hafrey1@users.noreply.github.com>
2025-09-06 22:19:40 +0800

    Create encode.js

---
+++ encode.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const bs58 = require('bs58');
+
+const inputFile = path.join(__dirname, 'luna-tv-config.json');
+const outputFile = path.join(__dirname, 'encoded.txt');
+
+// 读取 JSON 文件
+const jsonData = fs.readFileSync(inputFile);
+
+// 转 Base58
+const base58Encoded = bs58.encode(jsonData);
+
+// 写入输出文件
+fs.writeFileSync(outputFile, base58Encoded);
+
+console.log(`Base58 编码已生成: ${outputFile}`);
