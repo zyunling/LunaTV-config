1e3b49eec2d55db1211012584b75e4e1686a1ed3
hafrey <56250572+hafrey1@users.noreply.github.com>
2025-09-18 17:26:53 +0800

    Update oauth.js

--- web-editor/oauth.js
+++ web-editor/oauth.js
@@ -1,6 +1,9 @@
 function oauthLogin() {
-    const clientID = 'Ov23ligWR1OA4D8xEHN4';
-    const redirectURI = 'https://hafrey1.github.io/LunaTV-config/web-editor/callback.html';
-    const url = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${redirectURI}`;
-    window.location.href = url;
+    const clientID = 'Ov23ligWR1OA4D8xEHN4';  // 你的 Client ID
+    const redirectURI = 'https://hafrey1.github.io/LunaTV-config/web-editor/callback.html';  // 授权回调 URL
+    const scope = 'repo';  // 请求访问仓库的权限
+    const state = 'random_string_for_security';  // 防止 CSRF 攻击
+
+    const url = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&scope=${scope}&state=${state}`;
+    window.location.href = url;  // 重定向到 GitHub 授权页面
 }
